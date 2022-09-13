4f3607d58a8eb8ede3bab1238339cbf3d131aee0
DESKTOP-P9F5Q9K\netjoy <zzzhaoou12@163.com>
2022-09-13 13:41:04 +0800

    添加http go封装

---
+++ app/utils/HttpClient.go
@@ -0,0 +1,92 @@
+package utils
+
+import (
+	"crypto/tls"
+	"github.com/go-resty/resty/v2"
+	"strings"
+	"time"
+)
+
+const httpClientTimeOut = 3
+const httpClientRetryCount = 3
+
+// HttpGetResJson get request and json response
+func HttpGetResJson(url string, queryParams map[string]string, result interface{}) (res *resty.Response, err error) {
+	client := resty.New()
+
+	client.SetTimeout(time.Second * httpClientTimeOut)
+	client.SetRetryCount(httpClientRetryCount)
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	res, err = client.R().
+		SetQueryParams(queryParams).
+		SetHeader("Accept", "application/json").
+		SetResult(result).
+		Get(url)
+
+	return res, err
+}
+
+// HttpSendFormResJson send formData and response json
+func HttpSendFormResJson(url, method string, formData map[string]string, result interface{}) (res *resty.Response, err error) {
+	client := resty.New()
+
+	client.SetTimeout(time.Second * httpClientTimeOut)
+	client.SetRetryCount(httpClientRetryCount)
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	req := client.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetHeader("Accept", "application/json").
+		SetFormData(formData).
+		SetResult(result)
+
+	switch strings.ToLower(method) {
+	case "post":
+		res, err = req.Post(url)
+	case "put":
+		res, err = req.Put(url)
+	case "patch":
+		res, err = req.Patch(url)
+	case "delete":
+		res, err = req.Delete(url)
+	case "options":
+		res, err = req.Options(url)
+	default:
+		res, err = req.Head(url)
+	}
+
+	return res, err
+}
+
+// HttpSendJsonResJson send json and response json
+func HttpSendJsonResJson(url, method string, body interface{}, result interface{}) (res *resty.Response, err error) {
+	client := resty.New()
+
+	client.SetTimeout(time.Second * httpClientTimeOut)
+	client.SetRetryCount(httpClientRetryCount)
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	req := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Accept", "application/json").
+		SetBody(body).
+		SetResult(result)
+
+	switch strings.ToLower(method) {
+	case "post":
+		res, err = req.Post(url)
+	case "put":
+		res, err = req.Put(url)
+	case "patch":
+		res, err = req.Patch(url)
+	case "delete":
+		res, err = req.Delete(url)
+	case "options":
+		res, err = req.Options(url)
+	default:
+		res, err = req.Head(url)
+	}
+
+	return res, err
+}
--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require gorm.io/gorm v1.23.8
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/robfig/cron v1.2.0
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/viper v1.12.0
 	gorm.io/driver/mysql v1.3.6
