83543dc4b88472aa833e1fa3774389b818ea0714
DESKTOP-P9F5Q9K\netjoy <zzzhaoou12@163.com>
2022-09-22 14:39:32 +0800

    快手数据入库

--- app/service/kuaishou/KuaishouService.go
+++ app/service/kuaishou/KuaishouService.go
@@ -1,9 +1,13 @@
 package kuaishou
 
 import (
+	"account_check/app/model"
 	"account_check/app/utils"
 	"account_check/app/vo"
 	"account_check/bootstrap/driver"
+	"encoding/csv"
+	"fmt"
+	"os"
 	"path"
 	"time"
 )
@@ -30,10 +34,9 @@ func GetBills(request map[string]string) {
 		//获得当前的csv文件
 		if path.Ext(file) == ".csv" {
 			//todo 读取csv文件并插入数据库中
-			//log.Println(file)
+			readBillCsv(file)
 		}
 	}
-
 }
 
 //getToken 获得快手token
@@ -51,4 +54,56 @@ func getToken(request map[string]string) string {
 	}
 
 	return utils.GetInterfaceToString(token)
+}
+
+func readBillCsv(filepath string) {
+	file, err := os.Open(filepath) //读取文件
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1
+	csvdata, err := reader.ReadAll() //读取全部数据
+	//账单数据
+	var bills []model.OrderBill
+	var numbers []string
+	var numbersModel []string
+
+	for index, item := range csvdata { //按行打印数据
+		if index < 1 {
+			continue
+		}
+		//获得所有
+		numbers = append(numbers, item[4])
+		bills = append(bills, model.OrderBill{
+			Number:     item[4],
+			TradeNo:    item[5],
+			TradeAt:    utils.StringToTime(item[1]),
+			Amount:     utils.GetInterfaceToInt(item[6]),
+			PlatformId: 2,
+		})
+	}
+	if len(numbers) <= 0 {
+		return
+	}
+	driver.GVA_DB.Model(&model.OrderBill{}).Where("number in ?", numbers).Pluck("number", &numbersModel)
+	//比较两数组的不同
+	newNumber, _ := utils.Arrcmp(numbers, numbersModel)
+	if len(newNumber) <= 0 {
+		return
+	}
+	var newBills []model.OrderBill
+	for _, item := range bills {
+		for _, value := range newNumber {
+			if item.Number == value {
+				newBills = append(newBills, item)
+			}
+		}
+	}
+
+	//将订单插入数据库中
+	if len(newBills) > 0 {
+		driver.GVA_DB.Create(newBills)
+	}
 }
